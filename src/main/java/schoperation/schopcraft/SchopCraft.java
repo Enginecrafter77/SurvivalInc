077bedd4ff3dffbdd5295748ad19ad802e954604
Schoperation <cschop1@gmail.com>
2018-04-21 01:37:55 -0400

    Version bump for release

--- src/main/java/schoperation/schopcraft/SchopCraft.java
+++ src/main/java/schoperation/schopcraft/SchopCraft.java
@@ -20,9 +20,9 @@ public class SchopCraft {
 	// Basic mod constants.
 	public static final String MOD_ID = "schopcraft";
 	public static final String MOD_NAME = "SchopCraft";
-	public static final String VERSION = "0.3.0";
+	public static final String VERSION = "0.3.1";
 	public static final String MCVERSION = "1.12";
-	public static final String DEPENDENCIES = "required-after:forge@[14.23.1.2555,)";
+	public static final String DEPENDENCIES = "required-after:forge@[14.23.1.2611,)";
 	public static final String RESOURCE_PREFIX = MOD_ID + ":"; // schopcraft:
 	
 	// Make an instance of the mod.
