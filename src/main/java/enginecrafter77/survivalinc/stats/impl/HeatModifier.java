3b3daf249ad41397e3d7b82c845502887d5a7768
Enginecrafter77 <hutiramichal@gmail.com>
2020-04-22 17:47:32 +0200

    Fixed armor heat modifier scaling
    The armor heat exchange scaling was finally fixed using "conductivity distribution vectors".

--- src/main/java/enginecrafter77/survivalinc/stats/impl/HeatModifier.java
+++ src/main/java/enginecrafter77/survivalinc/stats/impl/HeatModifier.java
@@ -12,6 +12,7 @@ import enginecrafter77.survivalinc.stats.StatCapability;
 import enginecrafter77.survivalinc.stats.StatTracker;
 import enginecrafter77.survivalinc.stats.modifier.DamagingModifier;
 import enginecrafter77.survivalinc.stats.modifier.FunctionalModifier;
+import enginecrafter77.survivalinc.stats.modifier.Modifier;
 import enginecrafter77.survivalinc.stats.modifier.ModifierApplicator;
 import enginecrafter77.survivalinc.stats.modifier.OperationType;
 import enginecrafter77.survivalinc.stats.modifier.PotionEffectModifier;
@@ -44,7 +45,7 @@ public class HeatModifier implements StatProvider {
 	public static final HeatModifier instance = new HeatModifier();
 	
 	public static Map<Block, Float> blockHeatMap = new HashMap<Block, Float>();
-	public static Map<ItemArmor.ArmorMaterial, Float> armorInsulation = new HashMap<ItemArmor.ArmorMaterial, Float>();
+	public static ArmorModifier armorInsulation = new ArmorModifier(new float[] {0.2F, 0.35F, 0.3F, 0.15F});
 	public static ModifierApplicator<EntityPlayer> targettemp = new ModifierApplicator<EntityPlayer>();
 	public static ModifierApplicator<EntityPlayer> exchangerate = new ModifierApplicator<EntityPlayer>();
 	public static ModifierApplicator<EntityPlayer> consequences = new ModifierApplicator<EntityPlayer>();
@@ -63,16 +64,16 @@ public class HeatModifier implements StatProvider {
 		HeatModifier.blockHeatMap.put(Blocks.LIT_PUMPKIN, 10F);
 		
 		// Armor heat isolation
-		HeatModifier.armorInsulation.put(ItemArmor.ArmorMaterial.LEATHER, 0.5F);
-		HeatModifier.armorInsulation.put(ItemArmor.ArmorMaterial.CHAIN, 1.1F);
-		HeatModifier.armorInsulation.put(ItemArmor.ArmorMaterial.IRON, 1.2F);
-		HeatModifier.armorInsulation.put(ItemArmor.ArmorMaterial.GOLD, 1.5F);
-		HeatModifier.armorInsulation.put(ItemArmor.ArmorMaterial.DIAMOND, 2.25F);
+		HeatModifier.armorInsulation.addArmorType(ItemArmor.ArmorMaterial.LEATHER, 0.3F);
+		HeatModifier.armorInsulation.addArmorType(ItemArmor.ArmorMaterial.CHAIN, 1.1F);
+		HeatModifier.armorInsulation.addArmorType(ItemArmor.ArmorMaterial.IRON, 1.2F);
+		HeatModifier.armorInsulation.addArmorType(ItemArmor.ArmorMaterial.GOLD, 1.5F);
+		HeatModifier.armorInsulation.addArmorType(ItemArmor.ArmorMaterial.DIAMOND, 2.25F);
 		
 		HeatModifier.targettemp.add(new FunctionalModifier<EntityPlayer>(HeatModifier::whenNearHotBlock), OperationType.OFFSET);
 		
-		HeatModifier.exchangerate.add(new FunctionalModifier<EntityPlayer>(HeatModifier::whenWearingArmor), OperationType.SCALE);
 		HeatModifier.exchangerate.add(new FunctionalModifier<EntityPlayer>(HeatModifier::applyWetnessCooldown), OperationType.SCALE);
+		HeatModifier.exchangerate.add(HeatModifier.armorInsulation, OperationType.SCALE);
 		
 		HeatModifier.consequences.add(new ThresholdModifier<EntityPlayer>(new PotionEffectModifier(MobEffects.WEAKNESS, 0), 25F, ThresholdModifier.LOWER));
 		HeatModifier.consequences.add(new ThresholdModifier<EntityPlayer>(new PotionEffectModifier(MobEffects.MINING_FATIGUE, 0), 20F, ThresholdModifier.LOWER));
@@ -192,20 +193,88 @@ public class HeatModifier implements StatProvider {
 		return heat;
 	}
 	
-	public static float whenWearingArmor(EntityPlayer player)
+	/**
+	 * Armor Modifier takes care of storing and computing conductivity vectors.
+	 * In short, conductivity vector is an array of (4) values, each associated
+	 * with a specific armor piece. The values 0, 1, 2 and 3 are associated to
+	 * the helmet, chestplate, leggings and boots respectively. When computing
+	 * the conductivity of the armor set, the values from the stored vectors
+	 * associated to armor materials are used as a multiplier to base conductivity (1F)
+	 * @see #ArmorModifier(float[])
+	 * @see #addArmorType(net.minecraft.item.ItemArmor.ArmorMaterial, float)
+	 * @author Enginecrafter77
+	 */
+	public static class ArmorModifier implements Modifier<EntityPlayer>
 	{
-		float buff = 1F;
-		for(ItemStack stack : player.getArmorInventoryList())
+		/** The number of armor slots */
+		protected static final int armorPieces = 4;
+		
+		/** A map mapping armor materials to their conductivity vectors */
+		protected Map<ItemArmor.ArmorMaterial, Float[]> materialmap;
+		
+		/**
+		 * When an armor modifier is created, it is initialized with a
+		 * <i>conductivityDistribution</i> vector. Conductivity distribution
+		 * serves as a base for creating each conductivityVectors. It describes
+		 * the distribution of the base conductivity value among the armor. For
+		 * example, a conductivity distribution vector {0.2, 0.35, 0.3, 0.15}
+		 * tells us that the helmet contributes 20% to the conductivity, the
+		 * chestplate contributes 35%, the leggings 30% and the boots 15%.
+		 */
+		protected final float[] conductivityDistribution;
+		
+		/**
+		 * @see #conductivityDistribution
+		 * @param conductivityDistribution The conductivity distribution vector
+		 */
+		public ArmorModifier(float[] conductivityDistribution)
 		{
-			if(stack.getItem() instanceof ItemArmor)
-			{
-				ItemArmor item = (ItemArmor)stack.getItem();
-				if(HeatModifier.armorInsulation.containsKey(item.getArmorMaterial()))
-				{
-					buff *= Math.pow(HeatModifier.armorInsulation.get(item.getArmorMaterial()), 0.25F);
-				}
-			}
+			this.materialmap = new HashMap<ItemArmor.ArmorMaterial, Float[]>();
+			this.conductivityDistribution = conductivityDistribution;
 		}
-		return buff;
+		
+		/**
+		 * Adds an armor type and computes the conductivity vector for it.
+		 * The conductivity vector is computed using the following equation:
+		 * <pre>
+		 * 	y[i] = a ^ x[i]
+		 * </pre>
+		 * Where <b>y</b> is the resulting conductivity vector, <b>a</b> is the
+		 * <i>conductivity</i> parameter, <b>x</b> is the {@link #conductivityDistribution}
+		 * and <b>i</b> is the index of the armor piece.
+		 * @param material The material to associate the conductivity with
+		 * @param conductivity The conductivity of the material (i.e. the multiplier used when the armor set is homogeneous <i>material</i>)
+		 */
+		public void addArmorType(ItemArmor.ArmorMaterial material, float conductivity)
+		{
+			Float[] conductivityVectorInstance = new Float[armorPieces];
+			for(int index = 0; index < armorPieces; index++)
+				conductivityVectorInstance[index] = (float)Math.pow(conductivity, this.conductivityDistribution[index]);
+			this.materialmap.put(material, conductivityVectorInstance);
+		}
+		
+		@Override
+		public boolean shouldTrigger(EntityPlayer target, float level)
+		{
+			return true;
+		}
+		
+		@Override
+		public float apply(EntityPlayer target, float current)
+		{
+			float buff = 1F;
+			int index = 0;
+			for(ItemStack stack : target.getArmorInventoryList())
+			{
+				if(stack.getItem() instanceof ItemArmor)
+				{
+					ItemArmor.ArmorMaterial material = ((ItemArmor)stack.getItem()).getArmorMaterial();
+					if(this.materialmap.containsKey(material))
+						buff *= this.materialmap.get(material)[index];
+				}
+				index++;
+			}
+			return buff;
+		}	
 	}
 }
