c983c9b847ec14606fec45a8e3d793d8669b4f1e
Enginecrafter77 <hutiramichal@gmail.com>
2020-04-23 13:52:29 +0200

    Fixed server crash due to client-only marked config

--- src/main/java/enginecrafter77/survivalinc/config/ModConfig.java
+++ src/main/java/enginecrafter77/survivalinc/config/ModConfig.java
@@ -6,8 +6,6 @@ import net.minecraftforge.common.config.ConfigManager;
 import net.minecraftforge.fml.client.event.ConfigChangedEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
 @Config(modid = SurvivalInc.MOD_ID, name = SurvivalInc.MOD_ID, category = "")
 public class ModConfig {
@@ -15,7 +13,6 @@ public class ModConfig {
 	// The values in the config file. Add on more as needed.
 	// Sub-categories
 	@Config.Name("client")
-	@SideOnly(Side.CLIENT)
 	public static final ClientConfig CLIENT = new ClientConfig();
 	
 	@Config.Name("heat")
