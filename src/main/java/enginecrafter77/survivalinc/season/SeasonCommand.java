c9088f0d417cf78abcfe61d388fb76cfe07bdd55
Enginecrafter77 <hutiramichal@gmail.com>
2020-08-21 20:40:24 +0200

    Fixed escaping unicode character in SeasonCommand
    According to stylecheck, it is not good to escape unicode characters. As such, I replaced it with $ sign and at the end, it gets replaced with the appropriate character.

--- src/main/java/enginecrafter77/survivalinc/season/SeasonCommand.java
+++ src/main/java/enginecrafter77/survivalinc/season/SeasonCommand.java
@@ -1,5 +1,8 @@
 package enginecrafter77.survivalinc.season;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
@@ -8,6 +11,7 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
 import net.minecraftforge.common.MinecraftForge;
 
 public class SeasonCommand extends CommandBase {
@@ -40,7 +44,7 @@ public class SeasonCommand extends CommandBase {
 		switch(args[0])
 		{
 		case "set":
-			if(args.length < 3) throw new CommandException("Missing season name\n" + this.getUsage(sender));
+			if(args.length < 3) throw new CommandException(this.getUsage(sender));
 			data.season = Season.valueOf(args[1]);
 			data.day = Integer.parseInt(args[2]);
 			sender.sendMessage(new TextComponentString("Set season to " + data.toString()));
@@ -49,22 +53,34 @@ public class SeasonCommand extends CommandBase {
 			break;
 		case "info":
 			float currentoffset = data.season.getTemperatureOffset(data.day);
-			TextComponentString text = new TextComponentString("\u00a7aCurrent season: \u00a76" + data.toString());
-			text.appendText(String.format("\n\u00a7aBase Temperature:\u00a7r %f", currentoffset));
-			text.appendText(String.format("\n\u00a7aPeak Temperature:\u00a7r %f", data.season.getPeakTemperatureOffset()));
-			text.appendText(String.format("\n\u00a7aSeason Length:\u00a7r %d", data.season.getLength()));
-			text.appendText(String.format("\n\u00a7aTemperature Inclination:\u00a7r %f", data.season.getTemperatureOffset(data.day + 1) - currentoffset));
+			ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+			PrintStream message = new PrintStream(buffer);
+			message.format("$aCurrent season:$r %s\n", data.toString());
+			message.format("$aSeason Length:$r %d\n", data.season.getLength());
+			message.format("$aCurrent Temperature Offset:$r %.03f\n", currentoffset);
+			message.format("$aPeak Temperature Offset:$r %f\n", data.season.getPeakTemperatureOffset());
+			message.format("$aTemperature Inclination:$r %.03f", data.season.getTemperatureOffset(data.day + 1) - currentoffset);
 			if(sender instanceof Entity)
 			{
 				BlockPos position = new BlockPos(sender.getPositionVector());
-				float temperature = server.getWorld(0).getBiome(position).getTemperature(position);
-				text.appendText(String.format("\n\u00a7aTemperature at \u00a7e%s:\u00a7r %f", position.toString(), temperature));
+				Biome biome = server.getWorld(0).getBiome(position);
+				float biometempdiff = biome.getTemperature(position) - biome.getDefaultTemperature();
+				message.format("\n$aNominal temperature in $e%s$a:$r %.02f ($7%s$r)", biome.getBiomeName().toUpperCase(), SeasonController.instance.biomeTemp.originals.get(biome), SeasonCommand.formatOffset("%.03f", currentoffset));
+				message.format("\n$aTemperature at $eX%d Y%d Z%d$a:$r %.02f ($7%s$r)", position.getX(), position.getY(), position.getZ(), biome.getTemperature(position), SeasonCommand.formatOffset("%.03f", biometempdiff));
 			}
-			sender.sendMessage(text);
+			message.close();
+			sender.sendMessage(new TextComponentString(buffer.toString().replace('$', '\u00a7'))); // Replace the $ sign with the minecraft formatting sign (Code 00A7)
 			break;
 		default:
-			break; // Will not happen
+			break; // Like that's ever gonna happen! What a load of...
 		}
 	}
 	
+	private static <NUM extends Number> String formatOffset(String formatting, NUM number)
+	{
+		StringBuilder builder = new StringBuilder();
+		if(number.floatValue() > 0) builder.append('+');
+		builder.append(String.format(formatting, number));
+		return builder.toString();
+	}
 }
