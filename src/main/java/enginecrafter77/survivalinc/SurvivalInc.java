04acab2893ab154fb7689e9a61503706ee2ed2b3
Enginecrafter77 <hutiramichal@gmail.com>
2020-08-25 12:19:40 +0200

    Added git-based automatic build versioning
    A gradle plugin gradle-git-version was included to avoid future "version bump" commits. Now, each build will be based on the current status of the git branch. Although the mod will lose the ability to be built without git metadata, it will result in fewer obnoxious "version bump" commits, and snapshot builds will be easily identifiable.

--- src/main/java/enginecrafter77/survivalinc/SurvivalInc.java
+++ src/main/java/enginecrafter77/survivalinc/SurvivalInc.java
@@ -14,15 +14,11 @@ import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = SurvivalInc.MOD_ID, name = SurvivalInc.MOD_NAME, version = SurvivalInc.VERSION, acceptedMinecraftVersions = SurvivalInc.MCVERSION, dependencies = SurvivalInc.DEPENDENCIES)
+@Mod(modid = SurvivalInc.MOD_ID)
 public class SurvivalInc {
 
 	// Basic mod constants.
 	public static final String MOD_ID = "survivalinc";
-	public static final String MOD_NAME = "Survival Inc.";
-	public static final String VERSION = "=VERSION=";
-	public static final String MCVERSION = "1.12";
-	public static final String DEPENDENCIES = "required-after:forge@[14.23.1.2611,)";
 	public static final String RESOURCE_PREFIX = MOD_ID + ":";
 
 	// Make an instance of the mod.
@@ -30,7 +26,7 @@ public class SurvivalInc {
 	public static SurvivalInc instance;
 
 	// Logger
-	public static final Logger logger = LogManager.getLogger(MOD_NAME);
+	public static final Logger logger = LogManager.getLogger(SurvivalInc.class.getSimpleName());
 
 	// Create proxies to load stuff correctly.
 	@SidedProxy(clientSide = "enginecrafter77.survivalinc.ClientProxy", serverSide = "enginecrafter77.survivalinc.CommonProxy")
