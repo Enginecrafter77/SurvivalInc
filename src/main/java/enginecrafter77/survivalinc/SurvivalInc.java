Enginecrafter77/SurvivalInc

commit 36c512938fe52aeff8d69848cfcb4e71015aff9c

Added option to change the HUD position.
The HUD position has got it's own configuration category now. Almost all parameters can be tuned, which makes creating really weird looking HUDs possible.

src/main/java/enginecrafter77/survivalinc/config/ClientConfig.java
@@ -3,15 +3,14 @@ package enginecrafter77.survivalinc.config;
 import net.minecraftforge.common.config.Config;
 
 @Config.LangKey("config.survivalinc:client")
-public class ClientConfig {
-	@Config.LangKey("config.survivalinc:client.barTransparency")
-	@Config.RangeDouble(min = 0, max = 1)
-	public double barTransparency = 0;
-	
+public class ClientConfig {	
 	@Config.LangKey("config.survivalinc:client.autumnLeafColor")
 	@Config.RangeDouble(min = 0)
 	public double[] autumnLeafColor = {1.2, 0.6, 0.8};
 	
 	@Config.LangKey("config.survivalinc:client.pulsatingGhosts")
 	public boolean pulsatingGhosts = true;
+	
+	@Config.Name("hud")
+	public HUDConfig hud = new HUDConfig();
 }

src/main/java/enginecrafter77/survivalinc/config/HUDConfig.java
@@ -1,0 +1,82 @@
+package enginecrafter77.survivalinc.config;
+
+import enginecrafter77.survivalinc.client.Direction2D;
+import net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType;
+import net.minecraftforge.common.config.Config;
+
+@Config.LangKey("config.survivalinc:client.hud")
+public class HUDConfig {
+	
+	@Config.LangKey("config.survivalinc:client.hud.moveArmorBar")
+	@Config.Comment("Enable moving the Armor bar up to free up space when heat is enabled")
+	public boolean moveArmorBar = true;
+	
+	@Config.LangKey("config.survivalinc:client.hud.moveAirBar")
+	@Config.Comment("Enable moving the Air bubble bar up to free up space when hydration is enabled")
+	public boolean moveAirBar = true;
+	
+	@Config.LangKey("config.survivalinc:client.hud.originX")
+	@Config.Comment("The X-axis origin of the HUD represented as a fraction from the current screen resolution")
+	@Config.RangeDouble(min = 0, max = 1)
+	public double originX = 0.5D;
+	
+	@Config.LangKey("config.survivalinc:client.hud.originY")
+	@Config.Comment("The X-axis origin of the HUD represented as a fraction from the current screen resolution")
+	@Config.RangeDouble(min = 0, max = 1)
+	public double originY = 1D;
+	
+	@Config.LangKey("config.survivalinc:client.hud.renderTrigger")
+	@Config.Comment({"The element type to render the HUD in.", "Only change this if you are absolutely sure about what you are doing."})
+	@Config.RequiresMcRestart
+	public ElementType renderTrigger = ElementType.ALL;
+	
+	@Config.LangKey("config.survivalinc:client.hud.heatBarDirection")
+	@Config.Comment({"The fill direction of the heat bar.", "For example, setting this to RIGHT means that the bar fills from left to right."})
+	public Direction2D heatBarDirection = Direction2D.RIGHT;
+	
+	@Config.LangKey("config.survivalinc:client.hud.hydrationBarDirection")
+	@Config.Comment({"The fill direction of the hydration bar.", "For example, setting this to RIGHT means that the bar fills from left to right."})
+	public Direction2D hydrationBarDirection = Direction2D.LEFT;
+	
+	@Config.LangKey("config.survivalinc:client.hud.heatBarX")
+	@Config.Comment("The X coordinate of the top left corner of the heat bar relative to the computed origin point")
+	public int heatBarX = -91;
+	
+	@Config.LangKey("config.survivalinc:client.hud.heatBarY")
+	@Config.Comment("The Y coordinate of the top left corner of the heat bar relative to the computed origin point")
+	public int heatBarY = -49;
+	
+	@Config.LangKey("config.survivalinc:client.hud.hydrationBarY")
+	@Config.Comment("The X coordinate of the top left corner of the hydration bar relative to the computed origin point")
+	public int hydrationBarX = 10;
+	
+	@Config.LangKey("config.survivalinc:client.hud.hydrationBarX")
+	@Config.Comment("The Y coordinate of the top left corner of the hydration bar relative to the computed origin point")
+	public int hydrationBarY = -49;
+	
+	@Config.LangKey("config.survivalinc:client.hud.sanityIconX")
+	@Config.Comment("The X coordinate of the top left corner of the sanity icon relative to the computed origin point")
+	public int sanityIconX = -8;
+	
+	@Config.LangKey("config.survivalinc:client.hud.sanityIconY")
+	@Config.Comment("The Y coordinate of the top left corner of the sanity icon relative to the computed origin point")
+	public int sanityIconY = -51;
+	
+	@Config.LangKey("config.survivalinc:client.hud.heatBarCapacity")
+	@Config.Comment("The number of individual icons in the heat bar")
+	@Config.RangeInt(min = 1)
+	public int heatBarCapacity = 10;
+	
+	@Config.LangKey("config.survivalinc:client.hud.heatBarSpacing")
+	@Config.Comment({"The spacing between the icons in the heat bar in pixels", "Positive values pull the icons apart, negative values cram the icons together"})
+	public int heatBarSpacing = -1;
+	
+	@Config.LangKey("config.survivalinc:client.hud.hydrationBarCapacity")
+	@Config.Comment("The number of individual icons in the hydration bar")
+	@Config.RangeInt(min = 1)
+	public int hydrationBarCapacity = 10;
+	
+	@Config.LangKey("config.survivalinc:client.hud.hydrationBarSpacing")
+	@Config.Comment({"The spacing between the icons in the hydration bar in pixels", "Positive values pull the icons apart, negative values cram the icons together"})
+	public int hydrationBarSpacing = -1;
+}

build.gradle
@@ -104,7 +104,7 @@ curseforge {
 		}
 	}
 	options {
-		debug = true
+		debug = System.getenv("CF_DEBUG") != null
 	}
 }
 

src/main/java/enginecrafter77/survivalinc/ClientProxy.java
@@ -25,16 +25,20 @@ import enginecrafter77.survivalinc.stats.impl.SanityRecord;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class ClientProxy extends CommonProxy {
+	public RenderHUD hud;
+	
 	@Override
 	public void preInit(FMLPreInitializationEvent event)
 	{
 		super.preInit(event);
-		
+		this.hud = new RenderHUD(ModConfig.CLIENT.hud.renderTrigger);
 		if(ModConfig.SEASONS.enabled) MinecraftForge.EVENT_BUS.register(LeafColorer.instance);
 	}
 	
@@ -42,44 +46,7 @@ public class ClientProxy extends CommonProxy {
 	public void init(FMLInitializationEvent event)
 	{
 		super.init(event);
-		
-		TextureResource newicons = new TextureResource(new ResourceLocation(SurvivalInc.MOD_ID, "textures/gui/staticons.png"), 18, 18);
-		TextureResource sanityicon = new TextureResource(new ResourceLocation(SurvivalInc.MOD_ID, "textures/gui/sanity.png"), 32, 16);
-		TranslateRenderFilter moveup = new TranslateRenderFilter(new Position2D(0, -10));
-		if(ModConfig.HEAT.enabled)
-		{
-			StatFillBar<SimpleStatRecord> bar = new StatFillBar<SimpleStatRecord>(HeatModifier.instance, Direction2D.RIGHT, new TexturedElement(newicons, 0, 0, 9, 9, true));
-			bar.addLayer(new TexturedElement(newicons, 9, 0, 9, 9, true), SimpleStatRecord::getNormalizedValue);
-			bar.setCapacity(10);
-			bar.setSpacing(-1);
-			RenderHUD.instance.addIndependent(bar, new ElementPositioner(0.5F, 1F, -91, -49));
-			RenderHUD.instance.addFilter(moveup, ElementType.ARMOR);
-		}
-		if(ModConfig.HYDRATION.enabled)
-		{
-			StatFillBar<SimpleStatRecord> bar = new StatFillBar<SimpleStatRecord>(HydrationModifier.instance, Direction2D.LEFT, new TexturedElement(newicons, 0, 9, 9, 9, true));
-			bar.addLayer(new TexturedElement(newicons, 9, 9, 9, 9, true), SimpleStatRecord::getNormalizedValue);
-			bar.setCapacity(10);
-			bar.setSpacing(-1);
-			RenderHUD.instance.addIndependent(bar, new ElementPositioner(0.5F, 1F, 10, -49));
-			RenderHUD.instance.addFilter(moveup, ElementType.AIR);
-		}
-		if(ModConfig.SANITY.enabled)
-		{
-			StatFillBar<SanityRecord> bar = new StatFillBar<SanityRecord>(SanityModifier.instance, Direction2D.UP, new TexturedElement(sanityicon, 0, 0, 16, 16, true));
-			bar.addLayer(new TexturedElement(sanityicon, 16, 0, 16, 16, true), SimpleStatRecord::getNormalizedValue);
-			bar.setCapacity(1);
-			RenderHUD.instance.addIndependent(bar, new ElementPositioner(0.5F, 1F, -8, -51));
-			RenderHUD.instance.addFilter(moveup, ElementType.SUBTITLES);
-		}
-		if(ModConfig.GHOST.enabled)
-		{
-			Predicate<StatTracker> isGhostActive = (StatTracker tracker) -> tracker.getRecord(GhostProvider.instance).isActive();
-			RenderHUD.instance.addIndependent(new GhostEnergyBar(), new ElementPositioner(0.5F, 1F, -91, -39));
-			RenderHUD.instance.addFilterToAll(new HideRenderFilter<StatTracker>(isGhostActive), ElementType.HEALTH, ElementType.AIR, ElementType.ARMOR, ElementType.FOOD);
-		}
-		
-		if(ModConfig.HEAT.enabled || ModConfig.HYDRATION.enabled) RenderHUD.instance.addFilterToAll(moveup, ElementType.CHAT);
+		this.constructHUD();
 	}
 	
 	@Override
@@ -87,7 +54,71 @@ public class ClientProxy extends CommonProxy {
 	{
 		super.postInit(event);
 		
-		if(RenderHUD.instance.isUseful()) MinecraftForge.EVENT_BUS.register(RenderHUD.instance);
+		if(this.hud.isUseful())
+		{
+			MinecraftForge.EVENT_BUS.register(this.hud);
+			MinecraftForge.EVENT_BUS.register(this); // For config reloading
+		}
+		
 		if(ModConfig.GHOST.enabled) MinecraftForge.EVENT_BUS.register(new RenderGhost());
+	}
+	
+	@SubscribeEvent
+	public void onConfigChanged(ConfigChangedEvent.PostConfigChangedEvent event)
+	{
+		if(!event.getModID().equals(SurvivalInc.MOD_ID)) return;
+		
+		String config = event.getConfigID();
+		SurvivalInc.logger.info("ConfigChangedEvent({})", config);
+		
+		this.hud.reset();
+		this.constructHUD();
+	}
+	
+	private void constructHUD()
+	{		
+		float origin_x = (float)ModConfig.CLIENT.hud.originX;
+		float origin_y = (float)ModConfig.CLIENT.hud.originY;
+		
+		TextureResource newicons = new TextureResource(new ResourceLocation(SurvivalInc.MOD_ID, "textures/gui/staticons.png"), 18, 18);
+		TextureResource sanityicon = new TextureResource(new ResourceLocation(SurvivalInc.MOD_ID, "textures/gui/sanity.png"), 32, 16);
+		TranslateRenderFilter moveup = new TranslateRenderFilter(new Position2D(0, -10));
+		if(ModConfig.HEAT.enabled)
+		{
+			StatFillBar<SimpleStatRecord> bar = new StatFillBar<SimpleStatRecord>(HeatModifier.instance, ModConfig.CLIENT.hud.heatBarDirection, new TexturedElement(newicons, 0, 0, 9, 9, true));
+			bar.addLayer(new TexturedElement(newicons, 9, 0, 9, 9, true), SimpleStatRecord::getNormalizedValue);
+			bar.setCapacity(ModConfig.CLIENT.hud.heatBarCapacity);
+			bar.setSpacing(ModConfig.CLIENT.hud.heatBarSpacing);
+			hud.addElement(bar, new ElementPositioner(origin_x, origin_y, ModConfig.CLIENT.hud.heatBarX, ModConfig.CLIENT.hud.heatBarY));
+			
+			if(ModConfig.CLIENT.hud.moveArmorBar) hud.addFilter(moveup, ElementType.ARMOR);
+		}
+		if(ModConfig.HYDRATION.enabled)
+		{
+			StatFillBar<SimpleStatRecord> bar = new StatFillBar<SimpleStatRecord>(HydrationModifier.instance, ModConfig.CLIENT.hud.hydrationBarDirection, new TexturedElement(newicons, 0, 9, 9, 9, true));
+			bar.addLayer(new TexturedElement(newicons, 9, 9, 9, 9, true), SimpleStatRecord::getNormalizedValue);
+			bar.setCapacity(ModConfig.CLIENT.hud.hydrationBarCapacity);
+			bar.setSpacing(ModConfig.CLIENT.hud.hydrationBarSpacing);
+			hud.addElement(bar, new ElementPositioner(origin_x, origin_y, ModConfig.CLIENT.hud.hydrationBarX, ModConfig.CLIENT.hud.hydrationBarY));
+			
+			if(ModConfig.CLIENT.hud.moveAirBar) hud.addFilter(moveup, ElementType.AIR);
+		}
+		if(ModConfig.SANITY.enabled)
+		{
+			StatFillBar<SanityRecord> bar = new StatFillBar<SanityRecord>(SanityModifier.instance, Direction2D.UP, new TexturedElement(sanityicon, 0, 0, 16, 16, true));
+			bar.addLayer(new TexturedElement(sanityicon, 16, 0, 16, 16, true), SimpleStatRecord::getNormalizedValue);
+			bar.setCapacity(1);
+			hud.addElement(bar, new ElementPositioner(origin_x, origin_y, ModConfig.CLIENT.hud.sanityIconX, ModConfig.CLIENT.hud.sanityIconY));
+			hud.addFilter(moveup, ElementType.SUBTITLES);
+		}
+		if(ModConfig.GHOST.enabled)
+		{
+			Predicate<StatTracker> isGhostActive = (StatTracker tracker) -> tracker.getRecord(GhostProvider.instance).isActive();
+			hud.addElement(new GhostEnergyBar(), new ElementPositioner(0.5F, 1F, -91, -39));
+			hud.addFilterToAll(new HideRenderFilter<StatTracker>(isGhostActive), ElementType.HEALTH, ElementType.AIR, ElementType.ARMOR, ElementType.FOOD);
+		}
+		
+		if((ModConfig.HEAT.enabled && ModConfig.CLIENT.hud.moveArmorBar) || (ModConfig.HYDRATION.enabled && ModConfig.CLIENT.hud.moveAirBar))
+			hud.addFilterToAll(moveup, ElementType.CHAT);
 	}
 }

src/main/java/enginecrafter77/survivalinc/client/GaugeBar.java
@@ -3,7 +3,6 @@ package enginecrafter77.survivalinc.client;
 import java.awt.Color;
 import java.awt.color.ColorSpace;
 import enginecrafter77.survivalinc.SurvivalInc;
-import enginecrafter77.survivalinc.config.ModConfig;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.util.ResourceLocation;
@@ -79,7 +78,7 @@ public class GaugeBar extends SimpleOverlayElement<Float> {
 	protected void calculateRGBColor(float fraction)
 	{
 		this.getColor(fraction).getColorComponents(ColorSpace.getInstance(ColorSpace.CS_sRGB), this.colorcomponents);
-		this.colorcomponents[3] = 1F - (float)ModConfig.CLIENT.barTransparency;
+		this.colorcomponents[3] = 1F;
 	}
 	
 	/**

src/main/java/enginecrafter77/survivalinc/client/Position2D.java
@@ -1,6 +1,8 @@
 package enginecrafter77.survivalinc.client;
 
 import java.util.EnumMap;
+import java.util.Map;
+
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Maps;
 
@@ -19,16 +21,22 @@ public class Position2D {
 		this.position = Maps.immutableEnumMap(map);
 	}
 	
+	protected Map<Axis2D, Integer> getPositionMap()
+	{
+		return this.position;
+	}
+	
 	public Position2D move(int x, int y)
 	{
-		x += this.position.get(Axis2D.HORIZONTAL);
-		y += this.position.get(Axis2D.VERTICAL);
+		Map<Axis2D, Integer> position = this.getPositionMap();
+		x += position.get(Axis2D.HORIZONTAL);
+		y += position.get(Axis2D.VERTICAL);
 		return new Position2D(x, y);
 	}
 	
 	public Position2D move(Direction2D direction, int steps)
 	{
-		EnumMap<Axis2D, Integer> map = new EnumMap<Axis2D, Integer>(this.position);
+		EnumMap<Axis2D, Integer> map = new EnumMap<Axis2D, Integer>(this.getPositionMap());
 		int pos = map.get(direction.axis);
 		pos += direction.getAxialDelta() * steps;
 		map.put(direction.axis, pos);
@@ -37,7 +45,7 @@ public class Position2D {
 	
 	public int getPositionOn(Axis2D axis)
 	{
-		return this.position.get(axis);
+		return this.getPositionMap().get(axis);
 	}
 	
 	public final int getX()
@@ -60,33 +68,56 @@ public class Position2D {
 			this.mposition = new EnumMap<Axis2D, Integer>(source.position);
 		}
 		
+		public void set(Axis2D axis, int value)
+		{
+			this.mposition.put(axis, value);
+		}
+		
+		public void setX(int x)
+		{
+			this.set(Axis2D.HORIZONTAL, x);
+		}
+		
+		public void setY(int y)
+		{
+			this.set(Axis2D.VERTICAL, y);
+		}
+		
 		public Position2D toImmutable()
 		{
 			return new Position2D(this.mposition);
 		}
 		
 		@Override
+		protected Map<Axis2D, Integer> getPositionMap()
+		{
+			return this.mposition;
+		}
+		
+		@Override
 		public int getPositionOn(Axis2D axis)
 		{
-			return this.mposition.get(axis);
+			return this.getPositionMap().get(axis);
 		}
 		
 		@Override
 		public Position2D move(int x, int y)
 		{
-			x += this.mposition.get(Axis2D.HORIZONTAL);
-			y += this.mposition.get(Axis2D.VERTICAL);
-			this.mposition.put(Axis2D.HORIZONTAL, x);
-			this.mposition.put(Axis2D.HORIZONTAL, y);
+			Map<Axis2D, Integer> position = this.getPositionMap();
+			x += position.get(Axis2D.HORIZONTAL);
+			y += position.get(Axis2D.VERTICAL);
+			position.put(Axis2D.HORIZONTAL, x);
+			position.put(Axis2D.HORIZONTAL, y);
 			return this;
 		}
 		
 		@Override
 		public Position2D move(Direction2D direction, int steps)
 		{
-			int pos = this.mposition.get(direction.axis);
-			pos += direction.getAxialDelta() * steps;
-			this.mposition.put(direction.axis, pos);
+			Map<Axis2D, Integer> position = this.getPositionMap();
+			int value = position.get(direction.axis);
+			value += direction.getAxialDelta() * steps;
+			position.put(direction.axis, value);
 			return this;
 		}
 	}

src/main/java/enginecrafter77/survivalinc/client/RenderHUD.java
@@ -2,9 +2,10 @@ package enginecrafter77.survivalinc.client;
 
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
+
 import enginecrafter77.survivalinc.stats.StatCapability;
 import enginecrafter77.survivalinc.stats.StatTracker;
 import net.minecraft.client.Minecraft;
@@ -16,27 +17,38 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 @SideOnly(Side.CLIENT)
-public class RenderHUD {
-	public static final RenderHUD instance = new RenderHUD(ElementType.ALL);
-	
-	protected final Map<OverlayElement<? super StatTracker>, ElementPositioner> elements;
+public class RenderHUD {	
+	protected final Collection<HUDEntry> elements;
 	protected final Map<ElementType, Collection<ElementRenderFilter<? super StatTracker>>> filters;
 	protected final ElementType trigger;
 	
 	public RenderHUD(ElementType trigger)
 	{
-		this.elements = new LinkedHashMap<OverlayElement<? super StatTracker>, ElementPositioner>();
 		this.filters = new HashMap<ElementType, Collection<ElementRenderFilter<? super StatTracker>>>();
+		this.elements = new LinkedList<HUDEntry>();
 		this.trigger = trigger;
 	}
 	
+	/**
+	 * @deprecated Naming, use {@link #addElement(OverlayElement, ElementPositioner)}
+	 * @param element
+	 * @param position
+	 */
+	@Deprecated
 	public void addIndependent(OverlayElement<? super StatTracker> element, ElementPositioner position)
 	{
-		this.elements.put(element, position);
+		this.addElement(element, position);
+	}
+	
+	public HUDEntry addElement(OverlayElement<? super StatTracker> element, ElementPositioner position)
+	{
+		HUDEntry entry = new HUDEntry(element, position);
+		this.elements.add(entry);
+		return entry;
 	}
 	
 	public void addFilter(ElementRenderFilter<? super StatTracker> filter, ElementType element)
-	{
+	{		
 		Collection<ElementRenderFilter<? super StatTracker>> filters = this.filters.get(element);
 		if(filters == null)
 		{
@@ -52,6 +64,12 @@ public class RenderHUD {
 		{
 			this.addFilter(filter, element);
 		}
+	}
+	
+	public void reset()
+	{
+		this.elements.clear();
+		this.filters.clear();
 	}
 	
 	/**
@@ -105,15 +123,52 @@ public class RenderHUD {
 	public void renderOverlayPost(RenderGameOverlayEvent.Post event)
 	{
 		StatTracker tracker = Minecraft.getMinecraft().player.getCapability(StatCapability.target, null);
+		ScaledResolution resolution = event.getResolution();
 		ElementType type = event.getType();
 		if(this.trigger == type)
 		{
-			for(Map.Entry<OverlayElement<? super StatTracker>, ElementPositioner> entry : this.elements.entrySet())
+			for(HUDEntry entry : this.elements)
 			{
-				Position2D position = entry.getValue().getPositionOn(event.getResolution());
-				entry.getKey().draw(position, event.getPartialTicks(), tracker);
+				entry.draw(resolution, tracker, event.getPartialTicks());
 			}
 		}
-		this.runEndFilters(event.getResolution(), type, tracker);
+		this.runEndFilters(resolution, type, tracker);
+	}
+	
+	public class HUDEntry
+	{
+		public final OverlayElement<? super StatTracker> element;
+		public final ElementPositioner positioner;
+		public final List<ElementRenderFilter<? super StatTracker>> filters;
+		
+		protected HUDEntry(OverlayElement<? super StatTracker> element, ElementPositioner positioner)
+		{
+			this.filters = new LinkedList<ElementRenderFilter<? super StatTracker>>();
+			this.positioner = positioner;
+			this.element = element;
+		}
+		
+		public HUDEntry addFilter(ElementRenderFilter<? super StatTracker> filter)
+		{
+			this.filters.add(filter);
+			return this;
+		}
+		
+		public void draw(ScaledResolution resolution, StatTracker tracker, float partialTicks)
+		{
+			boolean draw = true;
+			for(ElementRenderFilter<? super StatTracker> filter : this.filters)
+			{
+				if(!filter.begin(resolution, tracker)) draw = false;
+			}
+			
+			if(draw)
+			{
+				Position2D position = this.positioner.getPositionOn(resolution);
+				this.element.draw(position, partialTicks, tracker);
+			}
+			
+			filters.forEach((ElementRenderFilter<? super StatTracker> filter) -> filter.end(resolution, tracker));
+		}
 	}
 }

commit a7ddf1f9809a0e2b1bbd2737189a5f74f4e62f7d

Fixed dedicated server config crash
Dedicated server was crashing because HUDConfig (referenced by ClientConfig) was referencing Direction2D, which was marked as client only. This crash was introduced by commit 36c5129. Additionally, Survival Inc has probably been crashing dedicated servers all the way from 8d65349 (1 commit after v1.2.0), since GhostProvider used client-only code in common handler. Again, this was fixed in this commit by marking the handler client-only, thus removing it from server.

src/main/java/enginecrafter77/survivalinc/SurvivalInc.java
@@ -30,7 +30,7 @@ public class SurvivalInc {
 	// Create proxies to load stuff correctly.
 	@SidedProxy(clientSide = "enginecrafter77.survivalinc.ClientProxy", serverSide = "enginecrafter77.survivalinc.CommonProxy")
 	public static CommonProxy proxy;
-
+	
 	// Basic event handlers. All of the work is done in the proxies.
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event)

src/main/java/enginecrafter77/survivalinc/config/ClientConfig.java
@@ -1,8 +1,11 @@
 package enginecrafter77.survivalinc.config;
 
 import net.minecraftforge.common.config.Config;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 @Config.LangKey("config.survivalinc.client")
+@SideOnly(Side.CLIENT)
 public class ClientConfig {	
 	@Config.LangKey("config.survivalinc.client.autumnLeafColor")
 	@Config.Comment("The color multiplier of leaves during autumn and winter")

src/main/java/enginecrafter77/survivalinc/config/HUDConfig.java
@@ -3,8 +3,11 @@ package enginecrafter77.survivalinc.config;
 import enginecrafter77.survivalinc.client.Direction2D;
 import net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType;
 import net.minecraftforge.common.config.Config;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 @Config.LangKey("config.survivalinc.client.hud")
+@SideOnly(Side.CLIENT)
 public class HUDConfig {
 	
 	@Config.LangKey("config.survivalinc.client.hud.moveArmorBar")

src/main/java/enginecrafter77/survivalinc/config/ModConfig.java
@@ -6,15 +6,18 @@ import net.minecraftforge.common.config.ConfigManager;
 import net.minecraftforge.fml.client.event.ConfigChangedEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
-@Config(modid = SurvivalInc.MOD_ID, name = SurvivalInc.MOD_ID, category = "")
+@Config(modid = SurvivalInc.MOD_ID, category = "")
 public class ModConfig {
 	
 	@Config.Name("general")
 	public static final GeneralConfig GENERAL = new GeneralConfig();
 	
 	@Config.Name("client")
-	public static final ClientConfig CLIENT = new ClientConfig();
+	@SideOnly(Side.CLIENT)
+	public static ClientConfig CLIENT;
 	
 	@Config.Name("heat")
 	public static final HeatConfig HEAT = new HeatConfig();
@@ -33,6 +36,18 @@ public class ModConfig {
 	
 	@Config.Name("seasons")
 	public static final SeasonConfig SEASONS = new SeasonConfig();
+	
+	// Big oof. But whatever, it works.
+	static {
+		try
+		{
+			ModConfig.CLIENT = new ClientConfig();
+		}
+		catch(NoSuchFieldError | NoClassDefFoundError exc)
+		{
+			System.err.println("Dropping client configuration objects...");
+		}
+	}
 	
 	// This deals with changed the config values in Forge's GUI in-game.
 	// It also deals with syncing some config values from the server to the

src/main/java/enginecrafter77/survivalinc/ghost/GhostProvider.java
@@ -49,6 +49,8 @@ import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.eventhandler.Event.Result;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerRespawnEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class GhostProvider implements StatProvider<GhostEnergyRecord> {
 	private static final long serialVersionUID = -2088047893866334112L;
@@ -264,6 +266,7 @@ public class GhostProvider implements StatProvider<GhostEnergyRecord> {
 	 * @param event The movement input update event
 	 */
 	@SubscribeEvent
+	@SideOnly(Side.CLIENT)
 	public static void blockMovementWhileResurrecting(InputUpdateEvent event)
 	{
 		if(ModConfig.GHOST.resurrectionBlocksMovement)

src/main/resources/assets/survivalinc/lang/en_us.lang
@@ -116,8 +116,8 @@ survivalinc.hydration=Hydration
 survivalinc.hydration.tooltip=Settings regarding Hydration mechanics
 config.survivalinc.hydration.enable=Enable Hydration
 config.survivalinc.hydration.enable.tooltip=Determines if the hydration mechanics should take part in the game.
-config.survivalinc.hydration.drinkAmount=Sip Volume
-config.survivalinc.hydration.drinkAmount.tooltip=The amount of hydration recovered with each sip from water puddle
+config.survivalinc.hydration.sipVolume=Sip Volume
+config.survivalinc.hydration.sipVolume.tooltip=The amount of hydration recovered with each sip from water puddle/canteen
 config.survivalinc.hydration.sweatingMultiplier=Sweating Multiplier
 config.survivalinc.hydration.sweatingMultiplier.tooltip=The multiplier applied to passive drain when the sweating threshold heat is crossed
 config.survivalinc.hydration.sweatingThreshold=Sweating Threshold
@@ -126,6 +126,8 @@ config.survivalinc.hydration.passiveDrain=Passive Drain
 config.survivalinc.hydration.passiveDrain.tooltip=The rate at which hydration passively decreases
 config.survivalinc.hydration.startValue=Starting Value
 config.survivalinc.hydration.startValue.tooltip=The amount of hydration the player has upon spawn
+config.survivalinc.hydration.canteenCapacity=Canteen Capacity
+config.survivalinc.hydration.canteenCapacity.tooltip=The amount of water stored in canteen, measured in sip count.
 
 # Sanity config
 survivalinc.sanity=Sanity