eb597b84e8a7e4ccecd0e5de294ff3f07a7c2ada
Enginecrafter77 <hutiramichal@gmail.com>
2021-02-15 12:12:10 +0100

    Fixed season command not working on server
    The season command was not crashing on server because the code used client-only I18n class for translations. The code was adjusted to use TextComponentTranslation instead.

--- src/main/java/enginecrafter77/survivalinc/season/SeasonCommand.java
+++ src/main/java/enginecrafter77/survivalinc/season/SeasonCommand.java
@@ -1,11 +1,8 @@
 package enginecrafter77.survivalinc.season;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 import java.util.List;
 
 import enginecrafter77.survivalinc.SurvivalInc;
-import net.minecraft.client.resources.I18n;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
@@ -15,7 +12,9 @@ import net.minecraft.entity.Entity;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.world.DimensionType;
 import net.minecraft.world.WorldServer;
 import net.minecraft.world.biome.Biome;
@@ -50,8 +49,9 @@ public class SeasonCommand extends CommandBase {
 		
 		if(args.length < 1) throw new WrongUsageException("Insufficient arguments\nUsgae: " + this.getUsage(sender));
 		
-		ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-		PrintStream message = new PrintStream(buffer);
+		ITextComponent seasonname = new TextComponentTranslation(season.getTranslationKey());
+		
+		TextComponentString message = null;
 		switch(args[0])
 		{
 		case "set":
@@ -60,58 +60,54 @@ public class SeasonCommand extends CommandBase {
 			if(target == null) throw new CommandException("Season \"" + args[1] + "\" not found.");
 			date.setSeason(target);
 			date.setDay(Integer.parseInt(args[2]));
-			message.format("Set calendar time to %s", date.toString());
+			message = new TextComponentString(String.format("Set calendar time to %s", date.toString()));
 			data.markDirty();
 			break;
 		case "advance":
 			int days = 1;
 			if(args.length >= 2) days = CommandBase.parseInt(args[1]);
 			date.advance(days);
-			message.format("Advancing season by %d day(s) --> %s", days, date.toString());
+			message = new TextComponentString(String.format("Advancing season by %d day(s) --> %s", days, date.toString()));
 			data.markDirty();
 			break;
 		case "info":
 			float currentoffset = SeasonController.instance.biomeTemp.getSeasonalTemperatureOffset(date);
 			SeasonCalendarDate next = date.clone();
-			next.advance(1);
-			
-			message.format("$aCurrent season:$r %s (Day %d)\n", localizeSeasonName(date.getCalendarEntry()), date.getDay());
-			message.format("$aSeason Length:$r %d\n", season.getLength());
-			message.format("$aTemperature Offset on $eDay %d$a:$r %.03f\n", date.getDay(), currentoffset);
-			message.format("$aPeak Temperature Offset in $e%s$a:$r %f\n", localizeSeasonName(date.getCalendarEntry()), season.getPeakTemperature());
-			message.format("$aCurrent Temperature Inclination:$r %.03f", SeasonController.instance.biomeTemp.getSeasonalTemperatureOffset(next) - currentoffset);
+			next.advance(1);			
+			message = new TextComponentString("§aCurrent season§r: ");
+			message.appendSibling(seasonname);
+			message.appendText(String.format(" (Day %d)\n§aSeason Length§r: %d\n§aTemperature Offset on §eDay %1$d§r: %.03f\n§aPeak Temperature Offset in §e", date.getDay(), season.getLength(), currentoffset));
+			message.appendSibling(seasonname);
+			message.appendText(String.format("§r: %f\n§aCurrent Temperature Inclination§r: %.03f", season.getPeakTemperature(), SeasonController.instance.biomeTemp.getSeasonalTemperatureOffset(next) - currentoffset));
 			if(sender instanceof Entity)
 			{
 				BlockPos position = new BlockPos(sender.getPositionVector());
 				Biome biome = server.getWorld(0).getBiome(position);
 				float biometempdiff = biome.getTemperature(position) - biome.getDefaultTemperature();
-				message.format("\n$aNominal temperature in current biome:$r %.02f ($7%s$r)", SeasonController.instance.biomeTemp.originals.get(biome), SeasonCommand.formatOffset("%.03f", currentoffset));
-				message.format("\n$aTemperature at $eX%d Y%d Z%d$a:$r %.02f ($7%s$r)", position.getX(), position.getY(), position.getZ(), biome.getTemperature(position), SeasonCommand.formatOffset("%.03f", biometempdiff));
+				message.appendText(String.format("\n§aNominal temperature in current biome§r: %.02f (§7%s§r)\n§aTemperature at §eX%d Y%d Z%d§r: %.02f (§7%s§r)", SeasonController.instance.biomeTemp.originals.get(biome), SeasonCommand.formatOffset("%.03f", currentoffset), position.getX(), position.getY(), position.getZ(), biome.getTemperature(position), SeasonCommand.formatOffset("%.03f", biometempdiff)));
 			}
 			break;
 		case "list":
 			List<SeasonCalendar.SeasonCalendarEntry> entries = date.getCalendarEntry().getCalendar().getSeasons();
-			message.print("$aAvailable seasons:$r");
+			message = new TextComponentString("§aAvailable seasons:§r");
 			for(SeasonCalendar.SeasonCalendarEntry entry : entries)
-				message.format("\n$e%s$r ($e%s$r): $e%d$r days$r", localizeSeasonName(entry), entry.getSeason().getName().toString(), entry.getSeason().getLength());
+			{
+				message.appendText("\n§e");
+				message.appendSibling(seasonname);
+				message.appendText(String.format("§r (§e%s§r): §e%d§r days", entry.getSeason().getName().toString(), entry.getSeason().getLength()));
+			}
 			break;
 		default:
 			// Like that's ever gonna happen! What a load of [sploosh] SOMEBODY ONCE TOLD ME THE WORLD IS GONNA ROLL ME...
 			throw new SyntaxErrorException("You can only choose one of the aforementioned options as the first argument!");
 		}
-		message.close();
-		sender.sendMessage(new TextComponentString(buffer.toString().replace('$', '\u00a7'))); // Replace the $ sign with the minecraft formatting sign (Code 00A7)
+		sender.sendMessage(message);
 		
 		if(data.isDirty())
 		{
 			SurvivalInc.proxy.net.sendToDimension(new SeasonSyncMessage(data), DimensionType.OVERWORLD.getId());
 			MinecraftForge.EVENT_BUS.post(new SeasonChangedEvent(world, date));
 		}
-	}
-	
-	private static String localizeSeasonName(SeasonCalendar.SeasonCalendarEntry entry)
-	{
-		return I18n.format(entry.getSeason().getTranslationKey(), new Object[0]);
 	}
 	
 	private static <NUM extends Number> String formatOffset(String formatting, NUM number)
