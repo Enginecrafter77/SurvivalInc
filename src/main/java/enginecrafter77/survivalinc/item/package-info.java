b33d7b255ad185d516bd5d50637acf0f1da89a9d
Enginecrafter77 <hutiramichal@gmail.com>
2022-11-27 01:01:37 +0100

    misc: Added package-info.java to items package

---
+++ src/main/java/enginecrafter77/survivalinc/item/package-info.java
@@ -0,0 +1,7 @@
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+package enginecrafter77.survivalinc.item;
+
+import mcp.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
